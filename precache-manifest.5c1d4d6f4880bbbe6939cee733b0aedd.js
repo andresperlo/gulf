95fc14d6251cbac8762fcedb37f80d0bc79a9b46
unknown <andres_perlo@gmail.com>
2020-09-21 14:48:34 -0300

    Updates

---
+++ precache-manifest.5c1d4d6f4880bbbe6939cee733b0aedd.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3103e4934ab65c4164b9b851061e778",
+    "url": "/gulf/index.html"
+  },
+  {
+    "revision": "b7b5085d8c57a30b487c",
+    "url": "/gulf/static/css/main.c98a9274.chunk.css"
+  },
+  {
+    "revision": "ce2b34d447f88ada6106",
+    "url": "/gulf/static/js/2.6b7c9363.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gulf/static/js/2.6b7c9363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b5085d8c57a30b487c",
+    "url": "/gulf/static/js/main.1c5b6d8d.chunk.js"
+  },
+  {
+    "revision": "ba7bdba978d40b92082b",
+    "url": "/gulf/static/js/runtime-main.7c12d458.js"
+  },
+  {
+    "revision": "6878b081bbbaf42d72d5215afa6e4941",
+    "url": "/gulf/static/media/aguasaborizada.6878b081.jpg"
+  },
+  {
+    "revision": "f57b7b1aada81e728544f76945da0754",
+    "url": "/gulf/static/media/asado.f57b7b1a.jpg"
+  },
+  {
+    "revision": "d227f9c9c6e67b4b261cca0eeb7a13b9",
+    "url": "/gulf/static/media/cafe.d227f9c9.jpeg"
+  },
+  {
+    "revision": "0d467b62d9020477922cb688e9816139",
+    "url": "/gulf/static/media/cafeconleche.0d467b62.jpg"
+  },
+  {
+    "revision": "7dc17db82e4f5eeaed592f12deb95aef",
+    "url": "/gulf/static/media/cerveza.7dc17db8.jpg"
+  },
+  {
+    "revision": "8f33005cf61ca706819cc5893dfdfe08",
+    "url": "/gulf/static/media/cortadito.8f33005c.jpg"
+  },
+  {
+    "revision": "20d3c38023004046917df4b693293755",
+    "url": "/gulf/static/media/empanadas.20d3c380.jpg"
+  },
+  {
+    "revision": "b3c4758ce15e2cb423b6e82d18084b75",
+    "url": "/gulf/static/media/gaseosas.b3c4758c.jpg"
+  },
+  {
+    "revision": "5bbb85bbf82d9c244a4583cb720aa716",
+    "url": "/gulf/static/media/humita.5bbb85bb.jpg"
+  },
+  {
+    "revision": "46094eb9a0579087843a2b3621acc40a",
+    "url": "/gulf/static/media/img1.46094eb9.jpg"
+  },
+  {
+    "revision": "c23dcc631a73029c2fe049a004c61a8c",
+    "url": "/gulf/static/media/logo.c23dcc63.png"
+  },
+  {
+    "revision": "a25bfcd2aa82939f8a053a5183ed8568",
+    "url": "/gulf/static/media/napo.a25bfcd2.jpg"
+  },
+  {
+    "revision": "cf10d342324e272ed01421e2b99cd255",
+    "url": "/gulf/static/media/sandwich.cf10d342.jpg"
+  },
+  {
+    "revision": "3cbd64505c8f738e720bff118b8682af",
+    "url": "/gulf/static/media/tamales.3cbd6450.jpg"
+  }
+]);
